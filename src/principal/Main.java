de479e3b385366a13ad16ffcb8ff9edde4b218ff
JoseP2005 <joseandres.pascualaviles@usp.ceu.es>
2023-12-11 11:07:17 +0100

    Ejercicio 47 sin corregir

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,11 +1,29 @@
 package principal;
-import paquete1.A;
+import java.util.ArrayList;
 public class Main {
+    private class A{
+        int atributo1, atributo2;
+        int metodo2(){
+            return atributo1 - atributo2;
+        }
+    }static void metodoEstatico(){
+        System.out.println("Esto es un método estático");
+    }
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        A a = new A();System.out.println(a);
+        ArrayList<A> arrayList;arrayList.add("cadena 1");
+        this.metodoEstatico();A.metodo2();
     }
 }
-// Cuando imprimes un objeto en Java sin proporcionar un método toString() personalizado,
+//Ejec 46: Cuando imprimes un objeto en Java sin proporcionar un método toString() personalizado,
 // se utiliza la implementación predeterminada de la clase Object,
 // que devuelve el nombre de la clase seguido por el identificador hexadecimal del objeto.
 // En este caso, la salida "paquete1.A@6d06d69c"
+
+
+// Ejec 47: Error: No has inicializado el ArrayList<A> antes de intentar agregar un elemento.
+//Error: Estás tratando de agregar una cadena ("cadena 1") a un ArrayList<A>, pero el tipo esperado es A
+//Error: No puedes usar this en un contexto estático. Elimina this. del método metodoEstatico()
+// y también para llamar al método metodo2() de la clase A.
